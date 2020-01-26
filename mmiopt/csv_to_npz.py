89e380d104b92d37de0f473d1f4c59db80ac1f13
TiresiasGit <cybarsatellites@gmail.com>
2020-01-26 22:05:33 +0900

    [TSP][MMI-OPT]csv_to_npz.py release.  - csv_to_npz.py      Use to convert csv to npz.                       ex: python csv_loader.py InFile.csv OutFile.npz

--- mmiopt/csv_to_npz.py
+++ mmiopt/csv_to_npz.py
@@ -1,17 +1,20 @@
 #coding: UTF-8
 # How to use this software?
-#  python csv_loader.py filename
+#  python csv_loader.py InFile.csv OutFile.npz
 
 import csv
 import sys
 import time
+import numpy as np
 
 def main() :
   argv = sys.argv
   with open(argv[1],"r") as f:
-    data = csv.reader(f)
-    line=[row for row in data]
-    print(line[0])
+    a = csv.reader(f,delimiter=',', quotechar='"')
+    data=[row for row in a]
+    print(data)
+    np.savez_compressed(argv[2],array_=data)
+    #print(np.load(argv[2])['array_'])
 
 if __name__ == "__main__":
   try:
